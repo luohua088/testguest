6b4117016eb8ec48741f293739d8a4b7befd1802
luohua <1321772563@qq.com>
2018-12-06 11:49:53 +0800

    221

--- testguest/settings.py
+++ testguest/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'testguest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'zhixin',
-        'PASSWORD': 'xiangduilun',
+        'NAME': 'testguest',
+        'USER': 'root',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
